e937fc286fddae7058e15b84060534986795b98a
gokadagaya <gokadagayathri@gmail.com>
2026-01-31 20:44:11 +0530

    First Assigment

---
+++ game_score.py
@@ -0,0 +1,9 @@
+name=input("Enter Players Name :")
+GamesPlayed=int(input("Enter Number of Games Played :"))
+total_score=int(input("Enter total score:"))
+avg_Score=total_score/GamesPlayed
+print("player :",name)
+print("Games PLayed:",GamesPlayed)
+print("Total Score:",total_score)
+print("AVERAGE Score:",avg_Score)
+

15605d38a2b4756db882ed158f659ae42e1ec108
gokadagaya <gokadagayathri@gmail.com>
2026-01-31 20:54:22 +0530

    First Assignment

--- game_score.py
+++ game_score.py
@@ -2,8 +2,8 @@ name=input("Enter Players Name :")
 GamesPlayed=int(input("Enter Number of Games Played :"))
 total_score=int(input("Enter total score:"))
 avg_Score=total_score/GamesPlayed
-print("player :",name)
-print("Games PLayed:",GamesPlayed)
+print("Player :",name)
+print("Games Played:",GamesPlayed)
 print("Total Score:",total_score)
-print("AVERAGE Score:",avg_Score)
+print("Average Score:",avg_Score)
 
